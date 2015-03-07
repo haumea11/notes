3ebb46f98e8f45f2ceda8fbe4a640bac11d66ce9
John Markiewicz <kuiperhaumea@gmail.com>
2015-03-07 14:05:36 -0500

    This commit is a commit.

--- latex/APCS/java/kochcurves/src/kochTester.java
+++ latex/APCS/java/kochcurves/src/kochTester.java
@@ -1,8 +1,8 @@
-import apcslib.*;
+//import apcslib.*;
 
 public class kochTester {
 	public static void main(String[] args){
-		SketchPad sp = new SketchPad(500, 500, 0);
+//		SketchPad sp = new SketchPad(500, 500, 0);
 		kochcurvetool kc = new kochcurvetool();
 		kc.drawKochCurve(3, 20);
 	}
