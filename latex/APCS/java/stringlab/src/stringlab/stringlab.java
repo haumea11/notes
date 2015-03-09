5346dcdccdcea76e0c2df1f4b483a1e3e0fcb2dd
John Markiewicz <kuiperhaumea@gmail.com>
2015-03-09 17:37:31 -0400

    Let's try -am, and also move some new math, english, and CS.

---
+++ latex/APCS/java/stringlab/src/stringlab/stringlab.java
@@ -0,0 +1,10 @@
+package stringlab;
+
+public class stringlab {
+	public static void main(String[] args){
+	String my_name = "John Almon Markiewicz";
+     // Sop out the length of the name.
+     System.out.println(my_name.length());
+
+	}
+}
