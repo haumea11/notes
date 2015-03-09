432c1c6d149177efaeb8ef500ea84d60a2fdd8b5
John Markiewicz <kuiperhaumea@gmail.com>
2015-03-09 17:37:21 -0400

    Let's try -am, and also move some new math, english, and CS.

--- latex/APCS/java/koch/src/kochTester.java
+++ latex/APCS/java/koch/src/kochTester.java
@@ -5,5 +5,9 @@ public class kochTester {
 //		SketchPad sp = new SketchPad(500, 500, 0);
 		kochcurvetool kc = new kochcurvetool();
 		kc.drawKochCurve(10, 200000);
-	}
+		
+		   String my_name = "John Almon Markiewicz";
+		     // Sop out the length of the name.
+		    System.out.println(my_name.length());
+
 }
