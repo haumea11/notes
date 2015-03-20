f7bcb7ded4c764eb835ac684c02c7caecd67af8f
John Markiewicz <markiewicz.john@gmail.com>
2015-03-20 14:25:32 -0400

    been a while

---
+++ latex/APCS/fix/piglatin/src/toPig.java
@@ -0,0 +1,40 @@
+import java.util.StringTokenizer;
+
+public class toPig{
+        public static String to_pig(String pig){
+                // If the word begins with a consonant, move the consonant to the
+                // end of the word.
+                //
+                // If the word begins with a vowel, add -"ay" to it.
+        	StringTokenizer ioo = new StringTokenizer (pig);
+        	String out = 
+                if(in.charAt(0) == 'a' || in.charAt(0) == 'e' || in.charAt(0) == 'i' || in.charAt(0) == 'o'
+                                || in.charAt(0) == 'u'){
+                        return(in+"ay");
+                }
+                // substring.  Use substring.
+                else if(!(in.charAt(0) == 'a' || in.charAt(0) == 'e' || in.charAt(0) == 'i' || in.charAt(0) == 'o'
+                        || in.charAt(0) == 'u') && !(in.charAt(1) == 'a' || in.charAt(1) == 'e' || in.charAt(1) == 'i' || in.charAt(1) == 'o'
+                        || in.charAt(1) == 'u') &&!(in.charAt(2) == 'a' || in.charAt(2) == 'e' || in.charAt(2) == 'i' || in.charAt(2) == 'o'
+                                || in.charAt(2) == 'u') ) {
+                	return(in.substring(3,(in.length()))+in.charAt(0)+in.charAt(1)+in.charAt(2)+"ay");
+                }
+                    
+                else if(!(in.charAt(0) == 'a' || in.charAt(0) == 'e' || in.charAt(0) == 'i' || in.charAt(0) == 'o'
+                        || in.charAt(0) == 'u') && !(in.charAt(1) == 'a' || in.charAt(1) == 'e' || in.charAt(1) == 'i' || in.charAt(0) == 'o'
+                        || in.charAt(0) == '1')){
+                	return(in.substring(2,(in.length()))+in.charAt(0)+in.charAt(1)+"ay");
+                }
+                
+                 else{
+                        return(in.substring(1,(in.length()))+in.charAt(0)+"ay");
+                }
+        }
+
+        public static void main(String[] args){
+               System.out.println( to_pig("apple"));
+               System.out.println(to_pig("pear"));
+               System.out.println(to_pig("prear"));
+               System.out.println(to_pig("Xylophone"));
+        }
+}
