c941c9ab907f87d3d3e49212b12458dc107c04eb
John Markiewicz <markiewicz.john@gmail.com>
2015-03-25 06:49:00 -0400

    I got my wm back working.  Anyway, here's a commit of more notes.

--- latex/APCS/fix/piglatin/src/toPig.java
+++ latex/APCS/fix/piglatin/src/toPig.java
@@ -1,13 +1,28 @@
+/*
+ * John Markiewicz
+ * 14.03.24
+ * The Piglatin lab
+ * 
+ * Here it is.
+ * 
+ * The most influential program
+ * *of all time*
+ * right here, right now.  A pig
+ * latin conversion utility.  
+ * Get excited.
+ * 
+ * Never got stringTokenizing to work.  
+ * Feel free to take off points for that.
+ */
 import java.util.StringTokenizer;
 
 public class toPig{
-        public static String to_pig(String pig){
+        public static String to_pig(String in){
                 // If the word begins with a consonant, move the consonant to the
                 // end of the word.
                 //
                 // If the word begins with a vowel, add -"ay" to it.
-        	StringTokenizer ioo = new StringTokenizer (pig);
-        	String out = 
+        	 
                 if(in.charAt(0) == 'a' || in.charAt(0) == 'e' || in.charAt(0) == 'i' || in.charAt(0) == 'o'
                                 || in.charAt(0) == 'u'){
                         return(in+"ay");
@@ -38,3 +53,10 @@ public class toPig{
                System.out.println(to_pig("Xylophone"));
         }
 }
+/*
+Output:
+appleay
+earpay
+earpray
+ophoneXylay
+	*/
